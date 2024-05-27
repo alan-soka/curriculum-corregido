2a72aa6567a71a252bb29f1cc3b5c08a11958609
JUANES <128440225+alan-soka@users.noreply.github.com>
2024-05-27 21:07:49 +0000

    luces red y blue a la escena 3d

--- three.js
+++ three.js
@@ -3,11 +3,11 @@ const container = document.getElementById('scene-container');
 
 // Crear la escena
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x000000); // Fondo negro para mejor contraste
+scene.background = new THREE.Color(0x212121); // Fondo negro para mejor contraste
 
 // Configurar la cámara
 const camera = new THREE.PerspectiveCamera(5, container.clientWidth / container.clientHeight, 0.1, 100);
-camera.position.z = 2;
+camera.position.z = 1.5;
 
 // Configurar el renderizador
 const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -22,24 +22,16 @@ controls.minDistance = -1;
 controls.maxDistance = 100;
 controls.maxPolarAngle = Math.PI / 2;
 
-// Agregar luces
-// Luz ambiental para iluminación general
-const ambientLight = new THREE.AmbientLight(0x000000);
-scene.add(ambientLight);
+// Luz direccional roja a la izquierda
+const redLight = new THREE.DirectionalLight(0xff0000, 2);
+redLight.position.set(-5, 5, -2).normalize();
+scene.add(redLight);
 
-// Luz direccional principal
-const directionalLight = new THREE.DirectionalLight(0xff0000, 2);
-directionalLight.position.set(5, 5, 5).normalize();
-scene.add(directionalLight);
+// Luz direccional azul a la derecha
+const blueLight = new THREE.DirectionalLight(0x0000ff, 2);
+blueLight.position.set(5, -5, -2).normalize();
+scene.add(blueLight);
 
-// Luz puntual para detalles adicionales
-const pointLight1 = new THREE.PointLight(0x000033, 2);
-pointLight1.position.set(10, 10, 10);
-scene.add(pointLight1);
-
-const pointLight2 = new THREE.PointLight(0x0000FF);
-pointLight2.position.set(-10, -10, -10);
-scene.add(pointLight2);
 
 // Aquí cargamos y agregamos el modelo 3D de la nave espacial utilizando OBJLoader
 const objLoader = new THREE.OBJLoader();
